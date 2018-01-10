d43b7223015f8de707e44dec54cfcd6087c1c35f
t3mp0ralflux <brenton.belanger@gmail.com>
2018-01-10 10:11:01 -0500

    yep

--- Project 1/Project 1/Program.cs
+++ Project 1/Project 1/Program.cs
@@ -1,19 +1,20 @@
 ﻿using System;
 using System.Data;
+using System.IO;
 using System.Text;
 
 namespace Project_1
 {
     public class Matrix
     {
-        public DataTable data;
+        public DataTable data = new DataTable();
         public int numRows;
-        public int cols;
-        public int[] size;
+        public int numCols;
+        public int[] size = new int[2];
 
         public int get_cols()
         {
-            return cols;
+            return numCols;
         }
 
         public int get_rows()
@@ -30,18 +31,37 @@ namespace Project_1
         {
             return size;
         }
-     
 
-        public int set_rows {get;set;}
+        public void set_size()
+        {
+            size[0] = numRows;
+            size[1] = numCols;
+        }
 
-        public int set_cols { get; set; }
+
+        public void set_numRows()
+        {
+            numRows = data.Rows.Count;
+        }
+
+        public void set_numCols()
+        {
+            numCols = data.Columns.Count;
+        }
+        public void set_data(DataTable dt)
+        {
+            data = dt;
+        }
 
         public string print_data()
+        // REDO ME
+
         {
             string dataPrint = string.Empty;
             StringBuilder sb = new StringBuilder();
-            foreach(DataRow dataRow in data.Rows) { 
-                foreach(var item in dataRow.ItemArray)
+            foreach (DataRow dataRow in data.Rows)
+            {
+                foreach (var item in dataRow.ItemArray)
                 {
                     sb.Append(item);
                     sb.Append(',');
@@ -52,24 +72,95 @@ namespace Project_1
             return dataPrint;
         }
 
-        public Matrix(DataTable dt)
+        public void importDataMatrix(string filepath, int startColumns)
+        {
+                
+            for (int col = 0; col < 2; col++)
+                {
+                    data.Columns.Add(new DataColumn());
+                }
+                string[] lines = File.ReadAllLines(filepath);
+
+            foreach (string line in lines)
+            {
+                var cols = line.Split('\t');
+                int f = 0;
+                DataRow dr = data.NewRow();
+                if (startColumns != 0)
+                {
+                    for (int cIndex = startColumns - 1; cIndex < startColumns + 1; cIndex++)
+                    {
+                     dr[f] = cols[cIndex];
+                        f++;
+                    }
+                    data.Rows.Add(dr);
+                }
+                else
+                {
+                    data.Rows.Add();
+                }
+            }
+            numCols = data.Columns.Count;
+            numRows = data.Rows.Count;
+            set_size();
+        }
+
+        public void print(Matrix one)
         {
             int i;
-            data = dt;
-            numRows = data.Rows.Count;
-            cols = data.Columns.Count;
-            size[0]=numRows;
-            size.SetValue(cols, 1);
-            for (i = 0; i < cols; i++)
+            int j;
+
+            for (i = 0; i < one.numRows; i++)
             {
-                data.Columns[i].DataType = typeof(Int32);
+                for (j = 0; j < one.numCols; j++)
+                {
+                    Console.Write("[" + one.data.Rows[i][j].ToString() + "]");
+                }
+                Console.Write("\n");
             }
         }
-        
+
+        public Matrix add(Matrix one, Matrix two, Matrix results)
+        {
+            int i;
+            int j;
+
+            if (one.numRows == two.numRows)
+            {
+                for (i = 0; i < one.numRows; i++)
+                {
+                    for (j = 0; j < one.data.Columns.Count; j++)
+                    {
+                        results.data.Rows[i][j] = Double.Parse(one.data.Rows[i][j].ToString()) + Double.Parse(two.data.Rows[i][j].ToString());
+                    }
+                }
+                return results;
+            }
+            else
+            {
+                return /*zero*/(results);
+            }
+        }
+
+        public static void Main()
+        {
+            string filepath = Directory.GetCurrentDirectory().ToString() + "\\2017 Fall Project 1 data.txt";
+            Matrix one = new Matrix();
+            Matrix two = new Matrix();
+            Matrix results = new Matrix();
+            one.importDataMatrix(filepath, 1);
+            two.importDataMatrix(filepath, 3);
+            results.importDataMatrix(filepath, 0);
+
+            results.add(one, two, results);
+
+            results.print(results);
+            Console.ReadKey();
+        }
 
     }
 
-    public class Operations
+/*   public class Operations
     {
         public static DataTable m_dataTableOne = new DataTable();
         public static DataTable m_dataTableTwo = new DataTable();
@@ -126,7 +217,7 @@ namespace Project_1
             int j;
             one = zero(one);
 
-            for(i=0; i<one.Rows.Count; i++)
+            for (i = 0; i < one.Rows.Count; i++)
             {
                 for (j = 0; j < one.Columns.Count; j++)
                     one.Rows[i][j] = 1;
@@ -137,7 +228,7 @@ namespace Project_1
         public static DataTable zero(DataTable one)
         {
             int i;
-            int j; 
+            int j;
             for (i = 0; i < one.Rows.Count; i++)
             {
                 for (j = 0; j < one.Columns.Count; j++)
@@ -161,9 +252,9 @@ namespace Project_1
             }
         }
 
-        public static void importData(string filepath)
+        public static void importDataMatrix(string filepath, int startColumns)
         {
-            for(int col = 0; col < 2; col++)
+            for (int col = 0; col < 2; col++)
             {
                 m_dataTableOne.Columns.Add(new DataColumn());
                 m_dataTableTwo.Columns.Add(new DataColumn());
@@ -171,13 +262,13 @@ namespace Project_1
             }
             string[] lines = System.IO.File.ReadAllLines(filepath);
 
-            foreach(string line in lines)
+            foreach (string line in lines)
             {
                 var cols = line.Split('\t');
 
                 DataRow dr = m_dataTableOne.NewRow();
                 DataRow dr2 = m_dataTableTwo.NewRow();
-                
+
                 for (int cIndex = 0; cIndex < 2; cIndex++)
                 {
                     dr[cIndex] = cols[cIndex];
@@ -191,26 +282,26 @@ namespace Project_1
                 m_dataTableTwo.Rows.Add(dr2);
             }
             DataRow dr3 = m_results.NewRow();
-            for (int rIndex = 0; rIndex<m_dataTableOne.Rows.Count; rIndex++)
+            for (int rIndex = 0; rIndex < m_dataTableOne.Rows.Count; rIndex++)
             {
                 m_results.Rows.Add();
             }
         }
 
         public static void Main()
-            {
-            string filepath = "E:\\Drive\\CSC340\\2017 Fall Project 1 data.txt";
+        {
+            string filepath = Directory.GetCurrentDirectory().ToString() + "\\2017 Fall Project 1 data.txt";
             importData(filepath);
 
             m_results = Operations.add(m_dataTableOne, m_dataTableTwo);
 
             print(m_results);
             Console.ReadKey();
-            }
+        }
 
 
 
-       
+
     }
-    
+    */
 }
